6903d3deb002e9a1bf778b54f52de0d6d608d73b
prashant naik <real.hyper.clocked@gmail.com>
2021-01-01 15:18:52 +0530

    Adding placeholder image to be loaded when there is no network

--- app/src/main/java/com/example/foody/bindingadapters/RecipesRowBinding.kt
+++ app/src/main/java/com/example/foody/bindingadapters/RecipesRowBinding.kt
@@ -39,6 +39,7 @@ class RecipesRowBinding {
         fun loadImageFromUrl(imageView : ImageView, imageUrl : String){
             imageView.load(imageUrl) {
                 crossfade(600)
+                error(R.drawable.ic_image_placeholder)
             }
         }
     }
