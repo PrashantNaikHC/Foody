c3393237bdd9d85a3cf0f1e3ca360d19383e0179
prashant naik <real.hyper.clocked@gmail.com>
2021-01-04 15:31:52 +0530

    Adding Datastore repository

--- app/src/main/java/com/example/foody/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/example/foody/viewmodels/RecipesViewModel.kt
@@ -3,6 +3,8 @@ package com.example.foody.viewmodels
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import com.example.foody.util.Constants
+import com.example.foody.util.Constants.Companion.DEFAULT_DIET_TYPE
+import com.example.foody.util.Constants.Companion.DEFAULT_MEAL_TYPE
 import com.example.foody.util.Constants.Companion.QUERY_API_KEY
 import com.example.foody.util.Constants.Companion.QUERY_DIET
 import com.example.foody.util.Constants.Companion.QUERY_FILL_INGREDIENT
@@ -17,8 +19,8 @@ class RecipesViewModel(application : Application) : AndroidViewModel(application
 
         query[QUERY_NUMBER] = "50"
         query[QUERY_API_KEY] = Constants.API_KEY
-        query[QUERY_TYPE] = "main course"
-        query[QUERY_DIET] = "gluten free"
+        query[QUERY_TYPE] = DEFAULT_MEAL_TYPE
+        query[QUERY_DIET] = DEFAULT_DIET_TYPE
         query[QUERY_RECIPE_INFORMATION] = "true"
         query[QUERY_FILL_INGREDIENT] = "true"
 
