90e4db0baf66703cdfe9a0605ff9dc083acc6e15
prashant naik <real.hyper.clocked@gmail.com>
2021-01-09 12:01:54 +0530

    Setting up webview in InstructionsFragment.kt

--- app/src/main/java/com/example/foody/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/example/foody/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import com.example.foody.R
+import com.example.foody.models.Result
+import com.example.foody.util.Constants
+import kotlinx.android.synthetic.main.fragment_instructions.view.*
 
 class InstructionsFragment : Fragment() {
 
@@ -14,7 +19,16 @@ class InstructionsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_instructions, container, false)
+
+        val result: Result? = arguments?.getParcelable(Constants.RECIPE_BUNDLE_RESULT)
+
+        val webViewUrl: String = result!!.sourceUrl
+        rootView.instructions_webview.apply {
+            webViewClient = object : WebViewClient() {}
+            loadUrl(webViewUrl)
+        }
+        return rootView
     }
 
 }
