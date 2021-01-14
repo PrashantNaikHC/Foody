830e39ea29c2545396d94353d8ade74cae029b25
prashant naik <real.hyper.clocked@gmail.com>
2021-01-14 23:22:49 +0530

    hooking up adapter with the favourite recipes fragment

--- app/src/main/java/com/example/foody/ui/fragments/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/example/foody/ui/fragments/FavoriteRecipesFragment.kt
@@ -5,16 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.foody.R
+import com.example.foody.adapters.FavouriteRecipesAdapter
+import com.example.foody.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_favorite_recipes.view.*
 
+@AndroidEntryPoint
 class FavoriteRecipesFragment : Fragment() {
+
+    val mAdapter: FavouriteRecipesAdapter by lazy { FavouriteRecipesAdapter() }
+    val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+
+        setupRecyclerView(rootView.favourite_recipes_recyclerview)
+        mainViewModel.readFavouriteRecipes.observe(viewLifecycleOwner, { favouritesList ->
+            mAdapter.setData(favouritesList)
+        })
+
+        return rootView
+    }
+
+    private fun setupRecyclerView(recyclerView: RecyclerView) {
+        recyclerView.adapter = mAdapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
     }
 
 }
